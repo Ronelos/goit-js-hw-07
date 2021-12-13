4c2f14cebd3ce5439c2789c383da536f74e2b82a
Andrii Kryvyi <87859325+Ronelos@users.noreply.github.com>
2021-12-13 23:55:51 +0200

    hw7-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,14 @@
 import { galleryItems } from './gallery-items.js';
+// const basicLightbox = require('basiclightbox');
+import * as basicLightbox from 'basiclightbox';
+
 const galleryEl = galleryItems.map((item) => `<div class="gallery__item"><img class="gallery__image" src=${item.preview}></div>`).join("");
 
 const gallery = document.querySelector(".gallery");
 gallery.insertAdjacentHTML('beforeend', galleryEl);
 
-console.log(galleryEl);
+// console.log(gallery);
 
-import * as basicLightbox from 'basiclightbox'
 
 const instance = basicLightbox.create(`
     <img src="assets/images/image.png" width="800" height="600">
