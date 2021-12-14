424bc027abd66b044d0dd3437e5cf2eaa19b2ba6
Andrii Kryvyi <87859325+Ronelos@users.noreply.github.com>
2021-12-14 23:20:17 +0200

    hw7-03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// const basicLightbox = require('basiclightbox');
-import * as basicLightbox from 'basiclightbox';
 
-const galleryEl = galleryItems.map((item) => `<div class="gallery__item"><img class="gallery__image" src=${item.preview}></div>`).join("");
+
+const galleryEl = galleryItems.map(({preview, original, description}) => `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`).join("");
+
+const fullScreen = event => {
+    event.preventDefault();
+    const isImg = event.target.classList.contains("gallery__image");
+    if (!isImg) return;
+    const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute("data-source")}"> `);
+
+instance.show()
+    console.log(event.target);
+}
 
 const gallery = document.querySelector(".gallery");
 gallery.insertAdjacentHTML('beforeend', galleryEl);
 
-// console.log(gallery);
+gallery.addEventListener('click', fullScreen);
 
 
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`)
 
-instance.show()
+
+
